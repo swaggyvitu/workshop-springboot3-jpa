66057a10a01ac99e90fac9de791812bb7ed9451d
Victor Fernandes <victor.fernandes2004@gmail.com>
2025-01-23 14:01:26 -0400

    Project created

---
+++ actv/src/main/java/com/swaggyvitu/actv/ActvApplication.java
@@ -0,0 +1,13 @@
+package com.swaggyvitu.actv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActvApplication.class, args);
+	}
+
+}
---
+++ actv/src/test/java/com/swaggyvitu/actv/ActvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaggyvitu.actv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
